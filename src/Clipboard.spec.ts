845b15a0977b28a464a084f0e289f3daa21a5997
Stefan Tenev <stefan.tenev@isobar.com>
2020-06-13 09:43:52 +0300

    So https://stackoverflow.com/questions/62351935/how-to-mock-navigator-clipboard-writetext-in-jest

---
+++ src/Clipboard.spec.ts
@@ -0,0 +1,27 @@
+// Object.assign(navigator, {
+//   clipboard: {
+//     writeText: () => {},
+//   },
+// });
+
+Object.defineProperty(navigator, "clipboard", {
+  value: {
+    writeText: () => {},
+  },
+});
+
+const dummyTextWriter = () => {
+  navigator.clipboard.writeText("zxc");
+};
+
+describe("Clipboard", () => {
+  describe("writeText", () => {
+    jest.spyOn(navigator.clipboard, "writeText");
+    beforeAll(() => {
+      dummyTextWriter();
+    });
+    it("should call clipboard.writeText", () => {
+      expect(navigator.clipboard.writeText).toHaveBeenCalledWith("zxc");
+    });
+  });
+});
